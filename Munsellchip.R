19bcaaa51b6e71031ff3fe1f5e6799bfcd654350
phil <philip.hughes@sydney.edu.au>
2012-07-04 16:21:36 +1000

    Got the euclidean function to work, still needs to be applied to all the data, then minimum distance found for each value.

--- Munsellchip.R
+++ Munsellchip.R
@@ -31,18 +31,17 @@ library(ggplot2)
 
 ##time to diverge from the munsell colour converting software.
 
-x<-read.table("Ctrial.txt", sep=",", header=T)
+LAB<-read.table("Ctrial.txt", sep=",", header=T)
 
 
-##This x needs to have each row matched with a munsell tile that is closest in colour. 
-##Euclidean distances would be the best method.
+
 
 
 
 mnsl_hues <- hue_slice(levels(mnsl_US$phcolor_colorhue))
 head(mnsl_US)
 
-###This is sebs work^
+###This is sebs work, converting LUV data to LAB
 
 mnsl_data<-mnsl_hues$data
 mnsl_LUV<-as.matrix(mnsl_data[,c("L","U","V")])
@@ -52,6 +51,57 @@ mnsl_LAB<-convertColor(mnsl_LUV,"Luv","Lab")
 colconvert<-data.frame(mnsl_data$name,mnsl_LAB)
 names(colconvert)<-c("munsell","L","A","B")
 munsLAB.df<-colconvert[!is.na(colconvert$munsell),]
+
+##This x needs to have each row matched with a munsell tile that is closest in colour. 
+##Euclidean distances would be the best method.
+
+
+
+
+euc <- function(dat, x1, y1, z1){
+  
+  
+  x<-dat$Main_L
+  y<-dat$Main_A
+  z<-dat$Main_B
+  
+  answer <- sqrt((x-x1)^2+(y-y1)^2+(z-z1)^2)
+  
+  return(answer)
+  
+}
+
+y <- matrix(NA, )
+##refLAB <- munslab.df
+for(i in 1:nrow(munsLAB.df)){
+  L <- munsLAB.df$L[i]
+  A <- munsLAB.df$A[i]
+  B <- munsLAB.df$B[i]
+}
+  y[i,] <-euc(LAB,L,A,B)
+  
+#for(i in 1:nrow(mns_data)){
+#  L <- mnsl_data$L[i]
+#  U <- mnsl_data$U[i]
+#  V <- mnsl_data$V[i]
+    
+#  y[i,] <-euc(LAB,L,U,V) 
+  
+  
+  y<-euc(LAB,1,2,3)
+  
+  
+  which.min
+  
+  ysquare <- function(number)
+  {
+    answer <- number * number
+    return(answer)
+  }
+  square(2)
+  
+  
+  dist(x, mnsl_data, method = "euclidean", diag = FALSE, upper = FALSE, p = 2)
 
 munsl_cols <- with(mnsl_US, paste(phcolor_colorhue, " ", phcolor_colorvalue, "/", phcolor_colorchroma, sep=""))
 munsl_cols[munsl_cols=="NA NA/NA"] <- NA
