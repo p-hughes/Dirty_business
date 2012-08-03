a8d29306fd1ed2404ae936c90782267fc3926906
Sebastian <sebastian.campbell@sydney.edu.au>
2012-08-03 13:24:05 +1000

    Modified NAtest to return dataframe and produce messages and warnings.

--- NAtest.R
+++ NAtest.R
@@ -1,12 +1,19 @@
-setwd("C:/Users/phug7649/Desktop/TXTBIN")
-NAtest <- function (file) {
-  test<-read.table(file,sep=",")
-  z<-na.exclude(test)
-  if(nrow(test)==nrow(z)) {
-    print("all is well")
-    } else {
-      print("WARNING: NA's present! ABORT ABORT")
-      }
-}
+NAtest <- function(file, ..., sep=",", header=TRUE, na.rm=TRUE){
+  
+  test <- read.table(file, sep=sep, header=header, ...)
+  orig_rows <- nrow(test)
+  
+  if(na.rm){
+    test <- na.exclude(test)
+    if(orig_rows == nrow(test)){
+      message("No NAs present: All is well")
+      } else {
+        warning("NAs present! ABORT ABORT")
+        }
+    }
+  
+  
+  return(test)
+  }
 # Line 3 if this goes astray: #test<-read.table("CarbonK1_nogap_top_K.txt",sep=",")
-NAtest("CarbonK1_nogap_top_K.txt")
+# NAtest("CarbonK1_nogap_top_K.txt")
