591e114e2d0ed83010ac97da69a7d79c51354d69
Sebastian <sebastian.campbell@sydney.edu.au>
2012-08-23 13:38:23 +1000

    Qhull now uses actual data. Converted it into a function.

--- qhull_algorithm.R
+++ qhull_algorithm.R
@@ -1,11 +1,7 @@
 library(plyr)
 
-set.seed(210812)
-rows <- 6000
-cols <- 10
-data_mat <- matrix(rnorm(cols*rows), ncol=cols)
-
-combs <- combn(ncol(data_mat), 2)
+original_data <- read.csv("original_data.csv")
+princomp_data <- read.csv("princomp_data.csv")
 
 #Function for alply
 
@@ -16,13 +12,41 @@ apply_hull_combination <- function(mat, combns){
   hull <- chull(mat[,i], mat[,j])
 }
 
-hull_list <- alply(combs, 2, apply_hull_combination, mat=data_mat)
+quick_hull <- function(data_mat){
 
-quick_hull_points <- unique(unlist(hull_list))
+  combs <- combn(ncol(data_mat), 2)
+  hull_list <- alply(combs, 2, apply_hull_combination, mat=data_mat)
 
+  quick_hull_points <- unique(unlist(hull_list))
+  
+  }
+
+sub_hull <- intersect(quick_hull(original_data), quick_hull(princomp_data))
+
+original_sub <- original_data[sub_hull, ]
+princomp_sub <- princomp_data[sub_hull, ]
+
+original_dist <- dist(original_sub)
+
+original.xy <- data.frame(t(combn(rownames(original_sub), 2)), as.numeric(original_dist))
+names(original.xy) <- c("p1", "p2", "distance")
+original.xy_sorted <- original.xy[order(original.xy$distance),]
+
+original.xy_sorted[(nrow(original.xy_sorted)%/%2-5):(nrow(original.xy_sorted)%/%2+5), ]
 
 # ##Qhull (really slow) version
 # library(geometry)
 # 
 # sample_hull <- convhulln(data_mat)
 # hull_points <- unique(as.vector(sample_hull))
+
+#BEFORE I FORGET:
+
+# Get the set of all euclidean distances (mahalanobis, maybe).
+# Find the values closest to the median (hoping these are the most evenly spaced distances.
+#    which should work, given these are all hull points.
+# Get the number of points we want and hey presto.
+# Maybe get furthest values as well? Maybe not. If points are evenly spaced, then no. If not, then yes?
+
+
+
