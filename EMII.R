634caca5f19e7f2fc693b797f455ab7f7a7a6b83
phil <philip.hughes@sydney.edu.au>
2012-08-31 14:50:52 +1000

    The script works over 1 iteration. In order for it to work for more, I need to attach the output to the original list(z) and calculate new distances.

--- EMII.R
+++ EMII.R
@@ -45,24 +45,24 @@ for (j in 1:ncol(combs)){
   refmax<-(which.max(finm))
   refmin<-(which.min(finm))
 
-  #namesmax<-refmax$names
- 
   ##getting maximum value and anchoring it to the row number in the master data set (z)
   BLARG<-rownames(z[cz,])==cz[refmax]
   BLARG<-as.matrix(cz[BLARG])
+    
+  ##getting minimum point and anchoring it to row number in master data set (z)
+  BLURG<-rownames(z[cz,])==cz[refmin]
+  BLURG<-as.matrix(cz[BLURG])
   
-  ##getting minimum point and anchoring it to row number in master data set
-  BLURG<rownames(z[cz.])==cz[refmin]
-  
-  ##retrieving all the principal component data from that row
+  ##retrieving all the principal component data from rows that contain maximum and minimum euclidean distances
   rowx<-z[BLARG,]
+  rowy<-z[BLURG,]
   
   ## retrieving all pc data from cz
   object<-z[cz,]
   
   ##getting distances
   pcdist<-as.matrix(point_euclid(object,rowx))
-  
+   
   ##creating the factor by which the yardstick length is modified
   factor<-4
   
@@ -73,8 +73,6 @@ for (j in 1:ncol(combs)){
   ##compare yardstick to the convex hull
   new <- ys < as.vector(pcdist)
   new<-as.matrix(new)
-  
- 
   
   ##Placing maximum (maxi) and minimum (origin) points in the final file
   
@@ -88,6 +86,19 @@ for (j in 1:ncol(combs)){
   
   ##Exclude any values inferior to yardstick (this file should be renamed cz when its time to reiterate)
   ans <- as.matrix(pcdist[new,])
+  
+  ##creating object for point max distance from origin
+  pcdisty<-as.matrix(point_euclid(z[row.names(ans),],rowy))
+  
+  ##second wheel of bicycle
+  
+  new <- ys < as.vector(pcdisty)
+  new<-as.matrix(new)
+  ans <- as.matrix(pcdisty[new,])
+  
+  
+  
+  
   ansII <- as.matrix 
   
   
