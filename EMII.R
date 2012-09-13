d23257610cf9eaea4a98d62cbfde962e895e94c8
phil <philip.hughes@sydney.edu.au>
2012-09-13 16:05:33 +1000

    Cleaned up. THERE IS STILL A BUG: Occasionally, the yardstick increases. This should not be possible.

--- EMII.R
+++ EMII.R
@@ -1,4 +1,8 @@
 
+                                              ##THE CONVEX BICYCLE##
+##A script made to identify a small number of points around the periphery of a data cloud. This should coincide with
+##the location of end members. It works by creating an n-dimensional convex hull (thanks seb),finding a point with a 
+##maximum distance from zero then finding the maximum distance of this point from all other points in the hull.
 
 
 ###Set working directory, read in the data###
@@ -8,41 +12,30 @@ setwd("C:/Users/phug7649/Desktop/TXTBIN")
 #z<-read.table("1_15 22_161534_top_14410_princ.txt", sep=",", na.strings="", header=TRUE)
 z<-read.table("Carbon_comp_6094.txt", sep=",", na.strings="", header=TRUE)
 ####Apply values to columns####
-#getwd()
-
 
 source(file.path(getwd(), "R-scripts", "point_euclid.R"))
 source(file.path(getwd(), "R-scripts", "qhull_algorithm.R"))
 
 
 z<- na.exclude(z)
-#combs <- combn(seq_len(ncol(z)), 2)
 
 ##creating a file to dump values
 file.create("bin.csv")
 bin<-matrix(NA, 1,1) 
-#names(bin)[2:3]<-c("row","distance")
 
-
-  
- # cz <- chull(zna)
-  cz<-quick_hull(z)
-  
-  ##added script for the "convex bicycle" 
-  
- 
-  
+##Using sebs script to create hulls
+cz<-quick_hull(z)                               ############
+                                                ##CONTROLS##
+                                                ############
+ys<-10      ##starting parameter for yardstick
+factor<-.5  ##creating the factor by which the yardstick length is modified
   
 ##I want the loop to start here
 
-
-
-ys<-10
 while (ys>0)
 
 {
   ##sum of rows
-  
   czr<-z[cz,]
   czr<-czr^2
   czrsum<-rowSums(czr)
@@ -51,15 +44,10 @@ while (ys>0)
   
   ##rows with max and min euclidean distance from zero
   refmax<-(which.max(finm))
-  #refmin<-(which.min(finm))
 
   ##getting maximum value and anchoring it to the row number in the master data set (z)
   BLARG<-rownames(z[cz,])==cz[refmax]
   BLARG<-as.matrix(cz[BLARG])
-    
-  ##getting minimum point and anchoring it to row number in master data set (z)
-  #BLURG<-rownames(z[cz,])==cz[refmin]
-  #BLURG<-as.matrix(cz[BLURG])
   
   ##retrieving all the principal component data from rows that contain maximum and minimum euclidean distances
   rowx<-z[BLARG,]
@@ -72,22 +60,14 @@ while (ys>0)
   ##getting distances
   pcdist<-as.matrix(point_euclid(object,rowx))
   #pcdisty<-as.matrix(point_euclid(object,rowy))
-   
-  ##creating the factor by which the yardstick length is modified
-  factor<-.5
-  
   
   ##yardstick
   b<-as.numeric(pcdist[which.max(pcdist),])
-  ys<-b*factor
-  
+  ys<-b*factor 
   
   ##compare yardstick to the convex hull
   new <- ys < as.vector(pcdist)
   new <- as.matrix(new)
-  
-  #newy <- ys < as.vector(pcdisty)
-  #newy <- as.matrix(newy)
   
   ##Placing maximum (maxi) and minimum (origin) points in the final file
   
@@ -101,48 +81,20 @@ while (ys>0)
 
   
   ##Exclude any values inferior to yardstick (this file should be renamed cz when its time to reiterate)
-  #ans <- as.matrix(pcdist[new,])
   finm <- as.matrix(pcdist[new,])
   
- 
+  ##Create a new object to replace the previous convex hull list
   cz<-as.numeric(rownames(finm))
-  #cz<-cz[finm[,1]]
-  
-  
-  
-  ##Check step. Hash this out when the whole thing works.
-  czo<-as.matrix(cz)
-  
-  ##same process, minimum values (second wheel).
-  
-  #czr<-z[cz,]
-  #czr<-czr^2
-  #czrsum<-rowSums(czr)
-  #fin<-sqrt(czrsum)
-  #finm<-as.matrix(fin)
-  #refmin<-(which.min(finm))
-  #BLURG<-rownames(z[cz,])==cz[refmin]
-  #BLURG<-as.matrix(cz[BLURG])
-  #rowy<-z[BLURG,]
-  #pcdisty<-as.matrix(point_euclid(object,rowy))
-  #newy <- ys < as.vector(pcdisty)
-  #newy <- as.matrix(newy)
-  #origin <- as.matrix(pcdisty == 0)
-  #or <- as.matrix(pcdisty[origin,])
-  #bin <- rbind(or,bin)
-  #max <- as.matrix(pcdisty==b)
-  #maxi <- as.matrix(pcdisty[max,])
-  #bin <-rbind(maxi,bin)
-  #finm <- as.matrix(pcdist[newy,])
-  #cz<-as.numeric(rownames(finm))
-  
   
   print(ys)
   
   
 }
+
+## removing duplicates
 a<-rownames(bin)
 s<-as.matrix(unique(a))
+
 
 
 
