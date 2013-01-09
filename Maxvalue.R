0ed3d6cb94b224e8c7ccb11f966459f6af042f76
phil <philip.hughes@sydney.edu.au>
2013-01-09 16:45:35 +1100

    minor changes

--- Maxvalue.R
+++ Maxvalue.R
@@ -1,9 +1,12 @@
 ##stupid little script to save me from the worlds largest if statement. Gets output from matlab and
 ##finds the largest value in each row.
 
-a<-read.table("matmem.csv",sep=",")
+#a<-read.table("matmem.csv",sep=",")
+#a<-read.table("edg_dist_2084.txt",sep=",",header=TRUE)
+setwd("C:/Users/phug7649/Desktop/TXTBIN")
+a<-read.table("std.txt",sep=",",header=TRUE)
 aa<-as.matrix(a)
 library(plyr)
 a$max<-apply(aa,1,which.max)
 head(a)
-write.csv(a, "matmem_max.csv")
+write.csv(a, "std.csv")
