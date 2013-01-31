b15dc03e08f7df8288cc03580d51100d54ec1867
phil <philip.hughes@sydney.edu.au>
2013-01-31 15:57:05 +1100

    Added numbers to letters

--- Maxvalue.R
+++ Maxvalue.R
@@ -3,10 +3,13 @@
 
 #a<-read.table("matmem.csv",sep=",")
 #a<-read.table("edg_dist_2084.txt",sep=",",header=TRUE)
+#a<-read.table("std.txt",sep=",",header=TRUE)
+
 setwd("C:/Users/phug7649/Desktop/TXTBIN")
-a<-read.table("std.txt",sep=",",header=TRUE)
+a<-read.table("Distances_2072.csv",sep=",",header=TRUE,check.names = FALSE)
 aa<-as.matrix(a)
 library(plyr)
 a$max<-apply(aa,1,which.max)
+a$max<-letters[a$max]
 head(a)
-write.csv(a, "std.csv")
+write.csv(a, "max_2072.csv")
