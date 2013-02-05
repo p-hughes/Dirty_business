01e4c882c230415416f6368f50c7471fc3f094dc
phil <philip.hughes@sydney.edu.au>
2013-02-05 16:18:41 +1100

    Added graphs, automated to work with matlab

--- post_meson.R
+++ post_meson.R
@@ -1,8 +1,13 @@
-##This is a script that takes the output from Matlab and combines it tinto a single table so it 
-##can be analysed.
+##This is a script that takes the output from Matlab and combines it into a single table so it 
+##can be analysed.This assumes the output from matlab is based on an id matrix of the same size 
+##as the centroid table.
 
+#Importing original principal components
 
-#setting working directory, reading in the data.
+setwd("C:/Users/phug7649/Desktop/TXTBIN")
+z<-read.table("Edgeroi_Fit_cut_PC_2072.csv",sep=",", na.strings="",header=TRUE)
+
+#setting working directory (in this case the edgeroi folders), reading in the data.
 
 setwd("C:/Users/phug7649/Desktop/edgeroi/New/W_200")
 #Soil attributes should go here
@@ -14,11 +19,13 @@ w<-read.csv("C:/Users/phug7649/Documents/MATLAB/weighting.csv",header=FALSE)
 names(dist)<-c('11a','11b','11c','11d','11e','11f','11g','11h','11i','11j','11k')
 
 
-
-##number of rows without the centroid table
-drows<-2072
+##number of rows in the data set
+rows<-read.csv("C:/Users/phug7649/Documents/MATLAB/rows.csv",header=FALSE)
+end<-read.csv("C:/Users/phug7649/Documents/MATLAB/end.csv",header=FALSE)
+cent<-read.csv("C:/Users/phug7649/Documents/MATLAB/cent.csv",header=FALSE)
+drows<-rows[1,1]-end[1,1]
 ##Number of rows in the centroid table
-crows<-11
+crows<-cent[1,1]
 ##making the table the correct length
 dist<-dist[1:drows,]
 ##adding the "max" column
@@ -51,7 +58,7 @@ summary
 write.csv(ATTRIB_DIST_cent, paste0('output_',w[1,1],'.csv'))
 write.csv(summary,paste0('summary_',w[1,1],'.csv'))
 
-
+##further analysis--DATA SPECIFIC!
 m1<-read.csv("summary_30.csv")
 m2<-read.csv("summary_60.csv")
 m3<-read.csv("summary_100.csv")
@@ -63,17 +70,93 @@ cout<-c_out[,3:7]
 tcout<-t(cout)
 plot(tcout[,1])
 for (i in 1:5){
-  plot(tcout[,i],ylim=c(1,200))
+  plot(tcout[,i],ylim=c(1,200),type="l", main=i)
 }
-# #may as well add principal components
-# a<-princomp(ATTRIB_DIST_cent[,2:13],scores=TRUE)
-# acomp<-cbind(d,comp)
-# 
-# comp<-a$scores/10
-# comp2<-a$sdev
-# head(comp)
-# 
-# ADc_comp1_2<-cbind(ATTRIB_DIST_cent,comp[,1:2])
-# test<-cbind(ADc_comp1_2,d)
+
+
+ #may as well add principal components, reading in the data and getting rid of that annoying 
+ #"X" column R sometimes makes
+
+ADc_comp1_2<- read.csv("output_30_pc.txt")
+ADc_comp1_2<-ADc_comp1_2[,setdiff(names(ADc_comp1_2), "Column.1")]
+ADc2_comp1_2<- read.csv("output_60.csv")
+ADc2_comp1_2<-ADc2_comp1_2[,2:ncol(ADc2_comp1_2)]
+AD_60_comp1_2<-cbind(ADc2_comp1_2,ADc_comp1_2[,26:27])
+
+
+#Time to use those PC's imported from earlier
+
+
+ecent<-ADc_comp1_2[2073:2083,]
+ecent$Soil.ID<-c("E","E","E","E","E","C","C","C","C","C","C")
+
+
+##Plotting time. I need two sets of plots, one from w=30 and the other from w=60+
+library(ggplot2)
+library(grid)
+
+##plot1
+ECENT <- ggplot(ADc_comp1_2, aes(x=Prin1, y=Prin2))+
+  theme_bw()+
+  geom_point(color='grey')+
+  geom_point(aes(x=Prin1, y=Prin2,shape=Soil.ID), data=ecent,size=6)+
+  scale_size_discrete("", range=c(4, 4)) +
+  theme(legend.text=element_text(size=20)) +
+  theme(axis.text.x=element_text(size=20))+
+  theme(axis.text.y=element_text(size=20))+
+  theme(axis.title.x = element_text(size=20))+
+  theme(axis.title.y = element_text(size=20))+
+  coord_equal()
+
+
+ECENT
+ggsave("ECENT.png",ECENT, type="cairo")
+
+
+
+###SEB GOING NUTS (more often referred to as a panel plot)
+
+##Plot1
+NUTS<-ggplot(ADc_comp1_2, aes(x=Prin1, y=Prin2), group=max)+
+  theme_bw() +
+  geom_point(data=ADc_comp1_2[,c("Prin1", "Prin2")], aes(x=Prin1, y=Prin2), inherit.aes=FALSE, colour="grey40")+
+  #geom_density2d()+
+  #geom_hex()+
+  #stat_binhex()+
+  #geom_point(colour=..count..)+
+  #stat_density2d(geom="tile", aes(fill = ..density..), contour = FALSE)+
+  stat_bin2d(binwidth=c(0.6, 0.6)) +
+  #geom_point(alpha=0.5)+
+  facet_wrap(~ max, nrow=3,)+
+  geom_point(data=ecent, aes( shape=Soil.ID), size=4, colour="black")+  
+  scale_fill_gradient(low="grey90", high="grey10")+
+  scale_shape_manual(values=c(1, 2))+
+  coord_equal()
+NUTS
+ggsave("NUTS.png",NUTS, type="cairo")
+
+##Plot2
+
+NUTS2<-ggplot(AD_60_comp1_2, aes(x=Prin1, y=Prin2), group=max)+
+  theme_bw() +
+  geom_point(data=AD_60_comp1_2[,c("Prin1", "Prin2")], aes(x=Prin1, y=Prin2), inherit.aes=FALSE, colour="grey40")+
+  #geom_density2d()+
+  #geom_hex()+
+  #stat_binhex()+
+  #geom_point(colour=..count..)+
+  #stat_density2d(geom="tile", aes(fill = ..density..), contour = FALSE)+
+  stat_bin2d(binwidth=c(0.6, 0.6)) +
+  #geom_point(alpha=0.5)+
+  facet_wrap(~ max, nrow=3,)+
+  geom_point(data=ecent, aes( shape=Soil.ID), size=4, colour="black")+  
+  scale_fill_gradient(low="grey90", high="grey10")+
+  scale_shape_manual(values=c(1, 2))+
+  coord_equal()
+NUTS2
+ggsave("NUTS2.png",NUTS2, type="cairo")
+
+## it looks as though clusters of about 1% total row length capture end point clusters. increasing w 
+## tidies up the data, but may miss extragrade clusters or worse, give data extreme weight.
+
  
 
