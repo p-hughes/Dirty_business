73140ab2deccadb85858324c73f0fa0539b93f66
Sebastian <sebastian.campbell@sydney.edu.au>
2013-02-13 17:05:14 +1100

    Improved qhull_algorithm so that it now takes an arbitrary number of dimensions at a time. Fixes #10. Added .png to .gitignore.

--- functions/qhull_algorithm.R
+++ functions/qhull_algorithm.R
@@ -1,44 +1,29 @@
 library(plyr)
 
-# original_data <- read.csv("original_data.csv")
-# princomp_data <- read.csv("princomp_data.csv")
-# 
-# #Function for alply
+quick_hull <- function(data_mat, choose=2){
 
-apply_hull_combination <- function(mat, combns){
-  i <- combns[1]
-  j <- combns[2]
+  combs <- combn(ncol(data_mat), choose)
   
-  hull <- chull(mat[,i], mat[,j])
-}
-
-quick_hull <- function(data_mat){
-
-  combs <- combn(ncol(data_mat), 2)
-  hull_list <- alply(combs, 2, apply_hull_combination, mat=data_mat)
+  if(choose <= 2){
+    hull_list <- alply(combs, 2, apply_hull_combination, mat=data_mat)
+  } else {
+    if(!require(geometry)) stop("Package 'geometry' required for combinations greater than 2")
+    hull_list <- alply(combs, 2, apply_convhull, mat=data_mat)
+  }
 
   quick_hull_points <- unique(unlist(hull_list))
   
 }
-# 
-# sub_hull <- intersect(quick_hull(original_data), quick_hull(princomp_data))
-# 
-# original_sub <- original_data[sub_hull, ]
-# princomp_sub <- princomp_data[sub_hull, ]
-# 
-# original_dist <- dist(original_sub)
-# 
-# original.xy <- data.frame(t(combn(rownames(original_sub), 2)), as.numeric(original_dist))
-# names(original.xy) <- c("p1", "p2", "distance")
-# original.xy_sorted <- original.xy[order(original.xy$distance),]
-# 
-# original.xy_sorted[(nrow(original.xy_sorted)%/%2-5):(nrow(original.xy_sorted)%/%2+5), ]
 
-# ##Qhull (really slow) version
-# library(geometry)
-# 
-# sample_hull <- convhulln(data_mat)
-# hull_points <- unique(as.vector(sample_hull))
+# #Functions for alply
+
+apply_hull_combination <- function(mat, combns){
+  hull <- chull(mat[,combns[1]], mat[,combns[2]])
+}
+
+apply_convhull <- function(mat, combns){
+  hull <- convhulln(mat[,as.numeric(combns)])
+}
 
 #BEFORE I FORGET:
 
