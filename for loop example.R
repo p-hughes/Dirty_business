6bbbeacf1dae0bdd925e976223c7740fb6861781
phil <philip.hughes@sydney.edu.au>
2013-03-21 17:14:04 +1100

    New example of a for loop in bottles of beer format

---
+++ for loop example.R
@@ -0,0 +1,10 @@
+for (i in 100:1){
+  
+  print(i)
+  print ("bottles of beer on the wall,")  
+  print (i)
+  print ("bottles of beer, take one down, pass it around,")
+  print (i-1)
+  print ("bottles of beer on the wall")
+  
+}
