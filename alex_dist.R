2a15c83ae5769f3be0d345428c7f8f2c8efb711d
phil <philip.hughes@sydney.edu.au>
2013-10-15 15:39:58 +1100

    got it workin! will sort out plots tomorrow.

--- alex_dist.R
+++ alex_dist.R
@@ -2,21 +2,11 @@ vince<-read.csv("C:\\Users\\phug7649\\Desktop\\TXTBIN\\CentInput.csv",sep=",",he
 
 #remove factors from vince
 
-spare<-vince
-re.fac<-spare[,2:ncol(spare)]
-
-# mean<-as.data.frame(colMeans(vince[,2:ncol(vince)]))
-# sx<-cov(cent.105.17[2:ncol(cent.105.17)])
-# D2<-mahalanobis(cent.105.17[2:ncol(cent.105.17)],mean,sx)
-
-arrayInd(which.max(dist(vince)),dim(dist(vince)))
-dist(vince)
 
 
-dist.vince<-dist(re.fac)
-dist.vince[1:10]
-which.min(dist.vince)
-dist.vince[20887]
+
+
+
 
 #For a set of k centroids, set a Mahalanobis distance cutoff dc- (mahalanobis is difficult for this data set)
 
@@ -24,37 +14,63 @@ dist.vince[20887]
 
 
 # 1. for each (i) of the k centroids find its closest neighbour (j) and its nearest neighbour distance dij. 
-# (This basically a list with three columns) 2. For the smallest distance dij in the list , combine the two 
+# (This basically a list with three columns) 
+#
+# 2. For the smallest distance dij in the list , combine the two 
 # centroids i and j by averaging them. Remove centroids I and j from the list and add a new centroid l with 
 # a concatenated name from the names of I and j separated by a colon (:). There are now k-1 centroids.
 # 
 # 3. Repeat 1 and 2 until all nearest neighbour distances are larger than dc.
 
+spare<-vince  ##so I can bugger up the object (vince) and not worry
+spare<-scale(spare[2:ncol(spare)])
+re.fac<-spare[,2:ncol(spare)]
 
-dc<-mean(dist.vince)
+## spare without the factor column
+dist.vince<-dist(re.fac) ## distance matrix of all the soil groups
+dc<-mean(dist.vince) ##mean of distances (I will use this as my completion criterion until told otherwise)
 
-dismat<-as.matrix(dist(re.fac))
-diag(dismat)<-NA
-min.dismat<-arrayInd(which.min(dismat),dim(dismat))
-max.dismat<-arrayInd(which.max(dismat),dim(dismat))
-str(min.dismat)
-min.dismat[1]
-min.dismat[2]
-re.fac[min.dismat[1],]<-mean(re.fac[min.dismat[1],],re.fac[min.dismat[2],])
-re.fac[min.dismat[2],]<-NA
+#arrayInd(which.max(dist(vince)),dim(dist(vince)))
+#dist(vince)
 
 
-while (which.min(dismat)<dc),{
+
+# dismat<-as.matrix(dist(re.fac))
+# diag(dismat)<-NA
+# min.dismat<-arrayInd(which.min(dismat),dim(dismat))
+# max.dismat<-arrayInd(which.max(dismat),dim(dismat))
+# str(min.dismat)
+# min.dismat[1]
+# min.dismat[2]
+# a<-ncol(spare)
+# spare[min.dismat[1],2:a]<-(re.fac[min.dismat[1],]+re.fac[min.dismat[2],])/2
+# spare[min.dismat[2],]<-NA
+# spare<-na.exclude(spare)
+# re.fac<-spare[,2:ncol(spare)]
+# 
+# nrow(spare)
+# nrow(vince)
+sum<-1
+factor<-3
+while (sum>0){
   
-  
-  
+  dismat<-as.matrix(dist(re.fac))
+  diag(dismat)<-NA
+  min.dismat<-arrayInd(which.min(dismat),dim(dismat))
+  max.dismat<-arrayInd(which.max(dismat),dim(dismat))
+  str(min.dismat)
+  min.dismat[1]
+  min.dismat[2]
+  a<-ncol(spare)
+  spare[min.dismat[1],2:a]<-(re.fac[min.dismat[1],]+re.fac[min.dismat[2],])/2
+  spare[min.dismat[2],]<-NA
+  spare<-na.exclude(spare)
+  re.fac<-spare[,2:ncol(spare)]
+  dcII<-which.min(dismat)*factor
+  sum<-dcII-dc
+   
   
 }
+nrow(spare)
 
 
-for (i in 1:nrow(vince)){
-  j<-arrayInd(which.min(dist(vince)),dim(dist(vince)))
-  m<-mean(j,i)
-  rbind(m,data)}
-  stop if m>dc
-               }
